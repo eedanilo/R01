c21d87da1c34fcc48bacbb9c17e9b0b4eee908ed
eedanilo <ewerthon.tavares@ccc.ufcg.edu.br>
2025-11-12 11:44:40 -0300

    Selection Sort implementado

--- src/main/java/sorting/simpleSorting/SelectionSort.java
+++ src/main/java/sorting/simpleSorting/SelectionSort.java
@@ -1,5 +1,5 @@
 package sorting.simpleSorting;
-
+import util.Util;
 import sorting.AbstractSorting;
 
 /**
@@ -11,7 +11,23 @@ public class SelectionSort<T extends Comparable<T>> extends AbstractSorting<T> {
 
 	@Override
 	public void sort(T[] array, int leftIndex, int rightIndex) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not Implemented yet!");
+		if (array == null || array.length == 0){
+			return;
+		}
+		if (leftIndex < 0 || rightIndex >= array.length || leftIndex >= rightIndex){
+			return;
+		}
+
+		for (int i = leftIndex; i < rightIndex; i++){
+			int min = i;
+			for (int j = i + 1; j <= rightIndex; j++){
+				if (array[j].compareTo(array[min]) < 0){
+					min = j;
+				}
+			}
+		if (i != min){
+			Util.swap(array, min, i);
+		}
+		}
 	}
 }
